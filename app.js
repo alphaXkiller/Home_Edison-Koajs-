beafa0cdaa23cbed96002051d2ce1651031c1aed
Franklin Huang <frankie0628@gmail.com>
2015-07-14 15:52:11 -0700

    init npm

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *(){
-  this.body = 'Hello from koajs';
+	console.log(this.request);
+	var url = this.request.url;
+	if(url === '/'){
+		this.body = 'Hello from koajs';
+	}
+	else if(url === '/date'){
+		this.body = new Date();
+	}
+	else{
+		this.status = 404;
+		this.body = 'Ooops! Page not found';
+	}
 });
 
-app.listen(80);
+app.listen(3000);
